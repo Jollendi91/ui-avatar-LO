bf40605eda1e4044f5e37c1c050c7d721bff2708
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-06 20:00:38 -0600

    create basic template for avatar component

---
+++ src/components/ui-avatar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="avatar">
+        <div class="indicator" :class="state"></div>
+        <p v-if="!image">{{ icon }}</p>
+        <img 
+            v-if="image"
+            :src="image" 
+            :alt="'avatar of' + label"
+            >
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+           label: String,
+           image: String,
+           state: String,
+           color: String 
+        },
+        computed: {
+            icon() {
+                return this.label.substring(0, 2).toUpperCase();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
