7c06cfab3a6a70a292d10116b14c08f4dace5500
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-07 06:47:55 -0600

    move dummy data to app.vue data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <ui-avatar 
-      label="Joshua Ollendick" 
-      image="https://c1.staticflickr.com/3/2813/33763063240_a966c3449e_q.jpg"
-      state="available" 
-      color="lightgreen"></ui-avatar>
+      :label="label"
+      :image="image"
+      :state="state"
+      :color="color"
+      ></ui-avatar>
   </div>
   
 </template>
@@ -13,6 +14,14 @@
   import Avatar from './components/ui-avatar.vue';
 
   export default {
+    data() {
+      return {
+        label: "Joshua Ollendick",
+        image: "https://c1.staticflickr.com/3/2813/33763063240_a966c3449e_q.jpg",
+        state: "available",
+        color: "lightgreen"
+      }
+    },
     components: {
       uiAvatar: Avatar
     }
