1b5a68fd691caa12da9507288849ec039f77da9b
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-06 21:39:15 -0600

    add styles

--- src/components/ui-avatar.vue
+++ src/components/ui-avatar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="avatar">
         <div class="indicator" :class="state"></div>
-        <p v-if="!image">{{ icon }}</p>
+        <p v-if="!image" :title="label">{{ icon }}</p>
         <img 
             v-if="image"
             :src="image" 
@@ -27,5 +27,40 @@
 </script>
 
 <style scoped>
+    .avatar {
+        font-size: 2.2em;
+        text-align: center;
+        line-height: 75px;
+        width: 75px;
+        height: 75px;
+        margin: 10px;
+        border-radius: 50%;
+        color: white;
+        background-color: grey;
+        position: relative;
+    }
 
+    .indicator, .default {
+        display: none;
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 15px;
+        height: 15px;
+        border: 2px solid white;
+        border-radius: 50%;
+        background-color: red;
+    }
+
+    .online {
+        background-color: rgb(0, 153, 255);
+    }
+
+    .available {
+        background-color: aquamarine;
+    }
+
+    .dnd {
+        background-color: coral;
+    }
 </style>
